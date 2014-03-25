25f32f16faa23b71cb742458a581130e68caa00d
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-03-25 23:50:41 +0100

    Increment default encryption iterations number

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var config = {};
 
 // Encryption algorithm to encrypt pastes. Available: aes256
-config.algorithm = 'aes256';
+config.algorithm  = 'aes256';
 
 // Default length for encryption keys
-config.keyLength = 8;
+config.keyLength  = 8;
 
-// Default number of iterations for encryption algorithms
-config.iterations = 12;
+// Default number of iterations for the selected encryption algorithm (if supported)
+config.iterations = 90510;
 
 // Mongodb connection data
 config.db = {
   host: 'localhost',
-  name: 'test'
+  name: 'pasteling'
 };
 
 module.exports = config;
