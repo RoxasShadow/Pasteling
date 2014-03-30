e47aa9a32567105de767ce4568f3abac72c5bada
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-03-30 04:05:13 +0200

    Entrust the loading of static resources to run.js

--- public/javascripts/run.js
+++ public/javascripts/run.js
@@ -25,5 +25,64 @@ var curl;
         config  : { loader: 'curl/loader/legacy', exports: 'CryptoJS' }
       }
     }
-  });
+  },
+
+  [
+    'js!/javascripts/lib/codemirror/lib/codemirror.js',
+    'js!/javascripts/lib/codemirror/addon/search/search.js',
+    'js!/javascripts/lib/codemirror/addon/search/searchcursor.js',
+    'js!/javascripts/lib/codemirror/addon/dialog/dialog.js',
+    'js!/javascripts/lib/codemirror/addon/display/fullscreen.js',
+    'js!/javascripts/lib/codemirror/mode/meta.js',
+
+    'css!/javascripts/lib/codemirror/lib/codemirror.css',
+    'css!/javascripts/lib/codemirror/theme/monokai.css',
+    'css!/javascripts/lib/codemirror/addon/dialog/dialog.css',
+    'css!/javascripts/lib/codemirror/addon/display/fullscreen.css'
+  ]).then(function() {
+    var textarea = document.getElementById('editor');
+    if(textarea != null) {
+      $editor = CodeMirror.fromTextArea(textarea, {
+        lineNumbers  : true,
+        lineWrapping : true,
+        matchBrackets: true,
+        readOnly     : $readonly,
+        mode         : $mode,
+        theme        : 'monokai',
+        extraKeys    : {
+          'F11': function(cm) {
+            cm.setOption('fullScreen', !cm.getOption('fullScreen'));
+          },
+
+          'Esc': function(cm) {
+            if(cm.getOption('fullScreen'))
+              cm.setOption('fullScreen', false);
+          }
+        }
+      });
+      $editor.setSize('100%', 700);
+
+      var langSelector = document.getElementById('langSelector');
+      if(langSelector)
+        langSelector.addEventListener('change', function(a) {
+          var selectedLang = this.options[this.selectedIndex].value;
+          var setLang      = function() { $editor.setOption('mode', selectedLang) };
+
+          var js = document.createElement('script');
+              js.type   = 'text/javascript';
+              js.src    = '/javascripts/lib/codemirror/mode/' + selectedLang + '/' + selectedLang + '.js';
+              js.onload = setLang;
+              js.onreadystatechange = setLang;
+
+          document.getElementsByTagName('head')[0].appendChild(js);
+        });
+      
+      var tabSelector = document.getElementById('tabSizeSelector')
+      if(tabSelector)
+        tabSelector.addEventListener('change', function(a) {
+          var selectedTabSize = this.options[this.selectedIndex].value;
+          $editor.setOption('tabSize', parseInt(selectedTabSize));
+        });
+    }
+  })
 }());
